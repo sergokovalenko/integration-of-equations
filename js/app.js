db9a50d2762542872f287d019db3388044be6450
fictional <kovalenkosergey.98@yandex.ru>
2018-11-30 22:56:04 +0400

    Calculation integral using parabola method

--- js/app.js
+++ js/app.js
@@ -17,6 +17,7 @@ const calculationsForRectangle = (a, h, n) => {
 const calculateIntegralUsingRectangleMethod = (a, b, n, eps = 0.001) => {
   let h = calcStep(a, b, n);
   let n1 = calculationsForRectangle(a, h, n);
+
   n *= 2;
   h = calcStep(a, b, n);
   let n2 = calculationsForRectangle(a, h, n);
@@ -37,12 +38,13 @@ const calculationsForTrapezium = (a, h, n, funcValues = {}) => {
 
   for (let i = 0; i <= n; i++) {
     let x = calcNextX2(a, i, h);
-    const funtionResult = 0;
+    let funtionResult = 0;
 
     if (calculatedValues[x] && typeof calculatedValues[x] === 'number') {
       funtionResult = calculatedValues[x];
     } else {
       funtionResult = F(x);
+      calculatedValues[x] = funtionResult;
     }
 
     if (i === 0 || i === n) {
@@ -80,35 +82,71 @@ const calculateIntegralUsingTrapeziumMethod = (a, b, n, eps = 0.001) => {
   return h;
 }
 
-const calculationsForParabola = (a, h, n) => {
+const calculationsForParabola = (a, h, n, funcValues = {}) => {
   let result = F(calcNextX2(a, 0, h));
+  const calculatedValues = { ...funcValues };
+
   for (let i = 1; i < n; i++) {
     let x = calcNextX2(a, i, h);
-    if (i % 2 === 1) {
-      result += 4 * F(x);
+    let funtionResult = 0;
+
+    if (calculatedValues[x] && typeof calculatedValues[x] === 'number') {
+      funtionResult = calculatedValues[x];
     } else {
-      result += 2 * F(x);
+      funtionResult = F(x);
+      calculatedValues[x] = funtionResult;
+    }
+
+    if (i % 2 === 1) {
+      result += 4 * funtionResult;
+    } else {
+      result += 2 * funtionResult;
     }
   }
 
   result += F(calcNextX2(a, n, h));
 
-  return result * h / 3;
+  return {
+    result: result * h / 3,
+    calculatedValues
+  };
 }
 
+const calculateIntegralUsingParabolaMethod = (a, b, n, eps = 0.001) => {
+  let h = calcStep(a, b, n);
+  let result;
+  let calculatedValues;
+  ({ result, calculatedValues }) = calculationsForParabola(a, h, n);
+  let n1 = result;
+  n *= 2;
+  h = calcStep(a, b, n);
+  ({ result, calculatedValues }) = calculationsForParabola(a, h, n, calculatedValues);
+  let n2 = result;
+
+  while (Math.abs(n2 - n1) > eps) {
+    n1 = n2;
+    n *= 2;
+    h = calcStep(a, b, n);
+    ({ result, calculatedValues }) = calculationsForParabola(a, h, n, calculatedValues);
+    n2 = result;
+  }
+
+  return h;
+}
+
+
 const calculateIntegral = (a, b, n, type) => {
-  const h = calcStep(a, b, n);
   let result = 0;
 
   switch (type) {
     case 0: 
-      result = calculationsForRectangle(a, h, n);
+      result = calculateIntegralUsingRectangleMethod(a, b, n);
       break;
     case 1:
-      result = calculationsForTrapezium(a, h, n);
+      result = calculateIntegralUsingTrapeziumMethod(a, b, n);
       break;
     case 2:
-      result = calculationsForParabola(a, h, n);
+      result = calculationsForParabola(a, b, n);
       break;
   }
 
